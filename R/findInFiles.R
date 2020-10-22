e30a370d30fa10e2e3313843826d647ab75a7dc9
Stéphane Laurent <laurent_step@outlook.fr>
2020-10-22 08:30:05 +0000

    version 0.1.0

---
+++ R/findInFiles.R
@@ -0,0 +1,163 @@
+#' Find pattern in files
+#' @description Find a pattern in some files.
+#'
+#' @param ext file extension, e.g. \code{"R"} or \code{"js"}
+#' @param pattern pattern to search for, a regular expression, e.g.
+#'   \code{"function"} or \code{"^function"}
+#' @param depth depth of the search, \code{NULL} or a negative number for an
+#'   entire recursive search (subdirectories, subdirectories of subdirectories,
+#'   etc.), otherwise a positive integer: \code{0} to search in the root
+#'   directory only, \code{1} to search in the root directory and its
+#'   subdirectories, etc.
+#' @param wholeWord logical, whether to match the whole pattern
+#' @param ignoreCase logical, whether to ignore the case
+#' @param perl logical, whether \code{pattern} is a Perl regular expression
+#' @param excludePattern a pattern; exclude from search the files and folders
+#'   which match this pattern
+#' @param excludeFoldersPattern a pattern; exclude from search the folders
+#'   which match this pattern
+#' @param root path to the root directory to search from
+#'
+#' @import htmlwidgets
+#' @export
+#'
+#' @examples library(findInFiles)
+#'
+#' folder <- system.file("example", package = "findInFiles")
+#' findInFiles("R", "function", root = folder)
+#'
+#' folder <- system.file("www", "shared", package = "shiny")
+#' findInFiles("css", "outline", excludePattern = "*.min.css", root = folder)
+findInFiles <- function(
+  ext, pattern, depth = NULL,
+  wholeWord = FALSE, ignoreCase = FALSE, perl = FALSE,
+  excludePattern = NULL, excludeFoldersPattern = NULL,
+  root = "."
+){
+
+  ansi <- paste0(grepInFiles(
+    ext = ext, pattern = pattern, depth = depth,
+    wholeWord = wholeWord, ignoreCase = ignoreCase, perl = perl,
+    excludePattern = excludePattern,
+    excludeFoldersPattern = excludeFoldersPattern,
+    directory = root
+  ), collapse = "\n")
+
+  # forward options using x
+  x = list(
+    ansi = ansi
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = "findInFiles",
+    x,
+    width = NULL,
+    height = NULL,
+    package = "findInFiles",
+    elementId = NULL
+  )
+}
+
+#' Shiny bindings for \code{findInFiles}
+#'
+#' Output and render functions for using \code{findInFiles} within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height a valid CSS unit (like \code{"100\%"},
+#'   \code{"400px"}, \code{"auto"}) or a number, which will be coerced to a
+#'   string and have \code{"px"} appended
+#' @param expr an expression that generates a '\code{\link{findInFiles}}' widget
+#' @param env the environment in which to evaluate \code{expr}
+#' @param quoted logical, whether \code{expr} is a quoted expression (with
+#'   \code{quote()})
+#'
+#' @name findInFiles-shiny
+#'
+#' @export
+#'
+#' @examples library(findInFiles)
+#' library(shiny)
+#'
+#' onKeyDown <- HTML(
+#'   'function onKeyDown(event) {',
+#'   '  var key = event.which || event.keyCode;',
+#'   '  if(key === 13) {',
+#'   '    Shiny.setInputValue(',
+#'   '      "pattern", event.target.value, {priority: "event"}',
+#'   '    );',
+#'   '  }',
+#'   '}'
+#' )
+#'
+#' ui <- fluidPage(
+#'   tags$head(tags$script(onKeyDown)),
+#'   br(),
+#'   sidebarLayout(
+#'     sidebarPanel(
+#'       selectInput(
+#'         "ext", "Extension",
+#'         choices = c("R", "js", "css")
+#'       ),
+#'       tags$div(
+#'         class = "form-group shiny-input-container",
+#'         tags$label(
+#'           class = "control-label",
+#'           "Pattern"
+#'         ),
+#'         tags$input(
+#'           type = "text",
+#'           class = "form-control",
+#'           onkeydown = "onKeyDown(event);",
+#'           placeholder = "Press Enter when ready"
+#'         )
+#'       ),
+#'       numericInput(
+#'         "depth", "Depth (set -1 for unlimited depth)",
+#'         value = 0, min = -1, step = 1
+#'       ),
+#'       checkboxInput(
+#'         "wholeWord", "Whole word"
+#'       ),
+#'       checkboxInput(
+#'         "ignoreCase", "Ignore case"
+#'       )
+#'     ),
+#'     mainPanel(
+#'       FIFOutput("results")
+#'     )
+#'   )
+#' )
+#'
+#'
+#' server <- function(input, output){
+#'
+#'   output[["results"]] <- renderFIF({
+#'     req(input[["pattern"]])
+#'     findInFiles(
+#'       ext = isolate(input[["ext"]]),
+#'       pattern = input[["pattern"]],
+#'       depth = isolate(input[["depth"]]),
+#'       wholeWord = isolate(input[["wholeWord"]]),
+#'       ignoreCase = isolate(input[["ignoreCase"]])
+#'     )
+#'   })
+#'
+#' }
+#'
+#' if(interactive()){
+#'   shinyApp(ui, server)
+#' }
+FIFOutput <- function(outputId, width = "100%", height = "400px"){
+  htmlwidgets::shinyWidgetOutput(
+    outputId, "findInFiles", width, height, package = "findInFiles"
+  )
+}
+
+#' @rdname findInFiles-shiny
+#' @export
+renderFIF <- function(expr, env = parent.frame(), quoted = FALSE){
+  if(!quoted){ expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, FIFOutput, env, quoted = TRUE)
+}
---
+++ R/grepInFiles.R
@@ -0,0 +1,77 @@
+isPositiveInteger <- function(x){
+  is.numeric(x) && (length(x) == 1L) && (floor(x) == x)
+}
+
+isString <- function(x){
+  is.character(x) && (length(x) == 1L)
+}
+
+getFiles <- function(ext, depth){
+  stopifnot(isPositiveInteger(depth))
+  wildcards <- Reduce(
+    file.path, x = rep("*", depth), init = paste0("*.", ext),
+    right = TRUE, accumulate = TRUE
+  )
+  Sys.glob(wildcards)
+}
+
+grepInFiles <- function(
+  ext, pattern, depth,
+  wholeWord, ignoreCase, perl,
+  excludePattern, excludeFoldersPattern,
+  directory
+){
+  if(Sys.which("grep") == ""){
+    stop("This package requires the 'grep' command-line utility.")
+  }
+  stopifnot(isString(ext))
+  stopifnot(isString(pattern))
+  stopifnot(is.logical(wholeWord))
+  stopifnot(is.logical(ignoreCase))
+  stopifnot(is.logical(perl))
+  wd <- setwd(directory)
+  on.exit(setwd(wd))
+  opts <- c("--colour=always", "-n")
+  if(wholeWord) opts <- c(opts, "-w")
+  if(ignoreCase) opts <- c(opts, "-i")
+  if(perl) opts <- c(opts, "-P")
+  if(!is.null(excludePattern)){
+    stopifnot(isString(excludePattern))
+    opts <- c(opts, paste0("--exclude=", shQuote(excludePattern)))
+  } #TODO: multiple patterns - https://stackoverflow.com/questions/41702134/grep-exclude-from-how-to-include-multiple-files
+  if(!is.null(excludeFoldersPattern)){
+    stopifnot(isString(excludeFoldersPattern))
+    opts <- c(opts, paste0("--exclude-dir=", shQuote(excludeFoldersPattern)))
+  }
+  # return(system2(
+  #   "grep",
+  #   args = c(
+  #     paste0("--include=\\*\\.", ext), opts, "-r", "-e", shQuote(pattern)
+  #   ),
+  #   stdout = TRUE, stderr = TRUE
+  # ))
+  results <- if(is.null(depth) || depth < 0){
+    suppressWarnings(system2(
+      "grep",
+      args = c(
+        paste0("--include=\\*\\.", ext), opts, "-r", "-e", shQuote(pattern)
+      ),
+      stdout = TRUE, stderr = TRUE
+    ))
+  }else{
+    files <- getFiles(ext, depth)
+    suppressWarnings(system2(
+      "grep",
+      args = c(shQuote(pattern), shQuote(files), opts),
+      stdout = TRUE, stderr = TRUE
+    ))
+  }
+  if(!is.null(status <- attr(results, "status"))){
+    if(status == 1){
+      message("No results.")
+    }else{
+      stop("An error occured. Possibly invalid 'grep' command.")
+    }
+  }
+  results
+}
